7ed8991edb03a06cb866b3f4d317915b55aab627
Damon Oehlman <damon.oehlman@gmail.com>
2011-10-26 20:17:17 +1000

    Added demos, tested in Chrome + Firefox

--- js/site.js
+++ js/site.js
@@ -2,5 +2,27 @@
     classtweak('pre', '+prettyprint +lang-js');
     prettyPrint();
     
+    $('.btn-run').live('click', function() {
+        var codeBlock = $(this).next('pre')[0];
+
+        if (codeBlock) {
+            // create a script tag
+            $('#bedazzle_demo').remove();
+            $('#bedazzle_stage').remove();
+            
+            $(this).before('<div id="bedazzle_stage"><div class="box"></div></div>');
+            
+            setTimeout(function() {
+                var script = document.createElement('script');
+                script.id = 'bedazzle_demo';
+                script.text = codeBlock.textContent;
+                
+                document.body.appendChild(script);
+            }, 100);
+        }
+        
+        return false;
+    });
+    
     $('pre').before('<a href="#" class="btn-run">run</a>');
 })();
