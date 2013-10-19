0fa0dbfd0f6d038ab0150032cd357ac49ca24068
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-19 17:03:28 +1100

    Moved demos -> examples for consistency with other modules

--- examples/loopy.js
+++ examples/loopy.js
@@ -3,6 +3,6 @@ var stylar = require('stylar');
 
 window.onload = function() {
   bedazzle('.spinner').x(50).rotate(180).end(function(elements) {
-      stylar(elements[0]).set('background', 'blue');
+    stylar(elements[0]).set('background', 'blue');
   }).loop();
 };
