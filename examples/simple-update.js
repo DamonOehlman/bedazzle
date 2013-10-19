a9d1b439057d23be3d8645598cd0b4fecda3d559
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-19 17:26:28 +1100

    Fixed examples

--- examples/simple-update.js
+++ examples/simple-update.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   require('..')('.box')
-    .frame.update('x200 y200 rotate-360')
-    .frame.update('x-200')
-    .frame.update('x200 y50 rotate360');
+    .frame.update({ x: 200, y: 200, rotate: 360 }) // 'x200 y200 rotate-360')
+    .frame.update({ x: -200 }) // 'x-200')
+    .frame.update({ x: 200, y: 50, rotate: 360 }) // 'x200 y50 rotate360');
 };
